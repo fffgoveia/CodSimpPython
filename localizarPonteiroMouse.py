907f605e76609977f9b7399b552b16252b892399
fffgoveia <115665368+fffgoveia@users.noreply.github.com>
2022-12-20 15:48:37 -0300

    Add files via upload

---
+++ localizarPonteiroMouse.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+time.sleep(3)
+x, y = pyautogui.position()
+print ("Posicao atual do mouse:")
+print ("x = "+str(x)+" y = "+str(y))
+
+#retorna True se x & y estiverem dentro da tela
+print ("\nEsta dentro da tela?")
+resp = pyautogui.onScreen(x, y)
+print (str(resp))
